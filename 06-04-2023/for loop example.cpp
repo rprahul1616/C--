e5a5d682d6af1b554e900a1d917ad95fb51b6839
Rahul Pramanik <rprahul1616@gmail.com>
2023-04-06 20:41:43 +0530

    second commit

--- 06-04-2023/for loop example.cpp
+++ 06-04-2023/for loop example.cpp
@@ -3,7 +3,18 @@ using namespace std;
 int main()
 {
 
+    int n,s,i ;
+    s = 0;
+    cout << "Enter a number " ;
 
+    cin >> n ;
+    for(i=1;i<=n;i++)
+    {
+        cout << i<<" ";
+        s=s+i;
+
+    }
+    cout << "\nsum = "<<s;
 
 
 
